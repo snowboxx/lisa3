0f0195d1ec52f101c5e24109b9a9f7f63cbdf426
snowboxx <36881031+snowboxx@users.noreply.github.com>
2018-11-05 01:46:23 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ client.on('ready', function(){
  game: { 
     type: 1,
      url: 'https://www.twitch.tv/skwadraa',
-    name: 'Watching You',
+    name: 'Watching Anime',
     application_id: '477187715658547201',
      assets: {
          large_image:   `${s[Math.floor(Math.random() * s.length)]}`,
@@ -21,4 +21,3 @@ client.on('ready', function(){
     }, 5000);
 });
 client.login(process.env.BOT_TOKEN);
-© 2018 GitHub, Inc.
